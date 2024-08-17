56426f35769dd7d539ab7ae0dfe4a7dcf4525685
jarrely <rellyy44@gmail.com>
2024-08-17 17:53:17 -0400

    Update app.py
    file path change

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import pandas as pd
 import math as mt
 import numpy as np
 import plotly.express as px
-vehicles=pd.read_csv("/Notebooks/vehicles_us.csv",sep=',')
+vehicles=pd.read_csv("vehicles_us.csv",sep=',')
 
 st.header("TripleTen Sprint 4 App", divider=True)
 #fig1= px.scatter(vehiclesv2, x='model_year', y='price', color='type')
